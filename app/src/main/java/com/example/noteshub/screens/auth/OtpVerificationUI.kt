71fc350bfa8d9c7772f38193cb8acb23365b3ba5
Jai Acharya <jaiacharya04@gmail.com>
2024-10-25 16:05:55 +0530

    Authentication UI

--- app/src/main/java/com/example/noteshub/screens/auth/OtpVerificationUI.kt
+++ app/src/main/java/com/example/noteshub/screens/auth/OtpVerificationUI.kt
@@ -1,6 +1,7 @@
 package com.example.noteshub.screens.auth
 
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -17,8 +18,11 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -27,15 +31,19 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.noteshub.R
 import com.example.noteshub.viewmodel.AuthViewModel
 
 @Composable
 fun OtpVerificationUI(navController: NavController, viewModel: AuthViewModel) {
-    var otpCode by remember { mutableStateOf("") }
+    val otpCodeList = remember { mutableStateListOf("", "", "", "", "", "") }
     var isOtpValid by remember { mutableStateOf(true) }
 
     // Observe the authentication state
     val authState by viewModel.authState.collectAsState()
+
+    // Create focus managers for each OTP field
+    val focusRequesterList = List(6) { FocusRequester() }
 
     Box(
         modifier = Modifier
@@ -48,70 +56,98 @@ fun OtpVerificationUI(navController: NavController, viewModel: AuthViewModel) {
                     )
                 )
             )
-    ){
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Enter OTP sent to your phone",color = Color.White,
-            fontSize = 26.sp)
-
-        Spacer(modifier = Modifier.height(170.dp))
-
-        OutlinedTextField(
-            value = otpCode,
-            colors = OutlinedTextFieldDefaults.colors(
-                Color.White,
-                focusedBorderColor = Color.White,
-                unfocusedBorderColor = Color.Gray,
-                cursorColor = Color.White,
-            ),
-            textStyle = TextStyle(fontSize = 18.sp),
-            shape = RoundedCornerShape(12.dp),
-            onValueChange = { otpCode = it },
-            label = { Text("OTP") },
-            isError = !isOtpValid,
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number, imeAction = ImeAction.Done),
+        Column(
             modifier = Modifier
-                .padding(start = 20.dp, end = 20.dp)
-                .fillMaxWidth()
-                .clip(RoundedCornerShape(12.dp))
-        )
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Button(
-            onClick = {
-                if (otpCode.length == 6) {
-                    viewModel.verifyOtp(otpCode)
-                } else {
-                    isOtpValid = false // Handle invalid OTP case
-                }
-            },
-            modifier = Modifier.fillMaxWidth(0.8f)
+                .fillMaxSize()
+                .padding(top = 92.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text(text = "Verify OTP")
-        }
+            Text(
+                text = "Enter OTP sent to your phone",
+                color = Color.White,
+                fontSize = 26.sp
+            )
 
-        if (!isOtpValid) {
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(text = "Invalid OTP", color = Color.Red)
-        }
+            Spacer(modifier = Modifier.height(130.dp))
 
-        // Handle navigation based on auth state
-        when (authState) {
-            is AuthViewModel.AuthState.Success -> {
-                navController.navigate("home")
+            Image(
+                painter = painterResource(id = R.drawable.otp_verification),
+                contentDescription = "Auth Screen",
+                modifier = Modifier.size(192.dp)
+            )
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                modifier = Modifier.fillMaxWidth().padding(horizontal = 36.dp)
+            ) {
+                for (i in 0 until 6) {
+                    OutlinedTextField(
+                        value = otpCodeList[i],
+                        colors = OutlinedTextFieldDefaults.colors(
+                            focusedContainerColor = Color.White,
+                            unfocusedContainerColor = Color.White,
+                            cursorColor = Color.Black,
+//                            focusedIndicatorColor = Color.Transparent, // Set indicator colors to transparent
+//                            unfocusedIndicatorColor = Color.Transparent
+                        ),
+                        textStyle = TextStyle(fontSize = 24.sp, color = Color.Black),
+                        shape = RoundedCornerShape(12.dp),
+                        onValueChange = { newValue ->
+                            if (newValue.length <= 1) {
+                                otpCodeList[i] = newValue
+                                if (newValue.isNotEmpty() && i < 5) {
+                                    // Move to the next field
+                                    focusRequesterList[i + 1].requestFocus()
+                                }
+                            } else if (newValue.isEmpty() && i > 0) {
+                                // Move to the previous field when backspace is pressed
+                                focusRequesterList[i - 1].requestFocus()
+                            }
+                        },
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number, imeAction = ImeAction.Done),
+                        modifier = Modifier
+                            .width(45.dp) // Width for each box
+                            .clip(RoundedCornerShape(12.dp))
+                            .focusRequester(focusRequesterList[i]) // Use FocusRequester correctly
+                    )
+                }
             }
-            is AuthViewModel.AuthState.Error -> {
-                // Show error message
-                Text(text = (authState as AuthViewModel.AuthState.Error).message, color = Color.Red)
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = {
+                    val otpCode = otpCodeList.joinToString("") // Combine the OTP digits
+                    if (otpCode.length == 6) {
+                        viewModel.verifyOtp(otpCode)
+                    } else {
+                        isOtpValid = false // Handle invalid OTP case
+                    }
+                },
+                modifier = Modifier.fillMaxWidth(0.8f)
+            ) {
+                Text(text = "Verify OTP")
             }
-            else -> {}
+
+            if (!isOtpValid) {
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(text = "Invalid OTP", color = Color.Red)
+            }
+
+            // Handle navigation based on auth state
+            when (authState) {
+                is AuthViewModel.AuthState.Success -> {
+                    navController.navigate("home")
+                }
+                is AuthViewModel.AuthState.Error -> {
+                    // Show error message
+                    Text(text = (authState as AuthViewModel.AuthState.Error).message, color = Color.Red)
+                }
+                else -> {}
+            }
         }
-    }
     }
 }
